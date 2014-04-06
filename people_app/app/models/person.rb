9b8cd3c17ba9f9d61cdbb15ccce6d020ee9dbc11
ilanaseid <ilanaseid@gmail.com>
2014-04-06 19:00:00 -0400

    have a drink tests pass

--- people_app/app/models/person.rb
+++ people_app/app/models/person.rb
@@ -10,4 +10,25 @@ def birthday
 	return birthday
 end
 
+def have_a_drink
+	age = Date.today.year - self.birthdate.year
+	@drinks = self.drinks
+	
+	if age < 21
+		return "Wait a few years"
+	elsif age >= 21 && @drinks <=2
+		return  self.drinks += 1
+	else 
+		return "Go home #{self.first_name}, you are drunk"
+	end
+end
+
+def drive_a_car
+	age = Date.today.year - self.birthdate.year
+
+	if age < 18
+		return "Not yet youngin"
+	end
+end
+
 end
--- people_app/spec/models/person_spec.rb
+++ people_app/spec/models/person_spec.rb
@@ -11,6 +11,8 @@ describe Person do
     birthdate: '1985-07-26'
   })}
 
+  junior =
+
   describe '#name' do
     it "display's the person's full name" do
       expect(ilana.name).to eq("Ilana Seid")
@@ -22,7 +24,34 @@ describe Person do
       expect(ilana.birthday).to eq("1985-07-26")
     end
   end
+
+  describe '#have_a_drink' do
+    it "should increase the number stored in the drinks attribute if they are over 21" do
+      ilana.have_a_drink
+     expect(ilana.drinks).to eq(3) 
+    end
+    it 'if they are under 21 then the string "Wait a few years" is returned' do
+      junior = Person.new(birthdate: '2013-01-01')
+      expect(junior.have_a_drink).to eq("Wait a few years")
+    end
+    it 'should return "Go home <object name> youre drunk", if someone is over 21 and has three drinks' do
+      alison = Person.new(first_name: "Alison", birthdate: '1977-01-01', drinks: 3)
+      expect(alison.have_a_drink).to eq("Go home Alison, you are drunk")
+    end
+  end
+
+  describe '#drive_a_car' do
+    it 'if they are under 18 then a string "Not yet youngin" is returned' do
+      junior = Person.new(birthdate: '2013-01-01')
+      expect(junior.drive_a_car).to eq("Not yet youngin")
+    end
+  end
+
 end
 
 
-
+# #drive_a_car`
+#   * 
+#   * if they are 18 and they have a license then they can drive 
+#   * if they are over 18 and have a license then they can drive
+#   * if they are over 21, have a license, and are drunk then the string "Looks like a cab for you tonight" is returned 
